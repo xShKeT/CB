31fdfcc8e64094b0a3f3ad75f5cf572606a1e1d2
Титов Артем Сергеевич <69TitovAS@region.cbr.ru>
2021-12-01 12:53:26 +0700

    test2

--- Test/src/test/java/Test1.java
+++ Test/src/test/java/Test1.java
@@ -1,16 +1,33 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import org.junit.jupiter.api.BeforeAll;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import io.qameta.allure.selenide.AllureSelenide;
 
 import java.util.concurrent.TimeUnit;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.FileDownloadMode.PROXY;
+import static com.codeborne.selenide.Selenide.*;
 
+import com.codeborne.selenide.testng.TextReport;
+
+@Listeners({TextReport.class})
 public class Test1 {
+    @BeforeAll
+    static void setupAllureReports() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+        );
+    }
     @Test
     public void test1() throws Exception {
         open("https://www.yandex.ru");
         $(".input__control").setValue("test").pressEnter();
-        TimeUnit.SECONDS.sleep(5);
+        Selenide.sleep(5000);
     }
 }
